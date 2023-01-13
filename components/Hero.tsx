94f45349b58a1f4d796449ef3d4c8f5cb68d166a
Kaushik das <kaushik1641.cse19@chitkara.edu.in>
2023-01-13 18:51:07 +0530

    p03

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,6 +44,9 @@ const Hero = (props: Props) => {
                     <Link href="#projects">
                         <button className='heroButton'>Projects</button>
                     </Link>
+                    <Link href="#contact">
+                        <button className='heroButton'>Contact</button>
+                    </Link>
                 </div>
             </div>
         </div>
