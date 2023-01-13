08579b9b1a270166bb1cf54cab2f09ba3bdd386f
Kaushik das <kaushik1641.cse19@chitkara.edu.in>
2023-01-13 14:39:09 +0530

    p05

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import Header from '../components/Header'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -30,9 +32,14 @@ export default function Home() {
         <WorkExperience />
       </section>
 
-      {/* Skills */}
+      <section id='skills' className='snap-start'>
+        <Skills />
+      </section>
 
       {/* Projects */}
+      <section id='projects' className='snap-start'>
+          <Projects />
+      </section>
 
       {/* Contact Me */}
 
